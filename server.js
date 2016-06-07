fe2c76266f3698ae32503a93f3e604bfd76ad713
Santosh Varma Kosuri J <santoshvarma.private@gmail.com>
2016-06-07 18:24:06 +0530

    added font awesome

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var ejs = require('ejs');
 var mongoose = require('mongoose');
+var favicon = require('express-favicon');
 
 //custom modules
 var router = require('./app_modules/routing.js');
@@ -13,11 +14,14 @@ var port = process.env.PORT || 3333;
 //db connection
 mongoose.connect('mongodb://santoshvarmakosurij:santoshvarmakosurij@ds021943.mlab.com:21943/santoshvarmakosurijblog');
 
+//favicon
+app.use(favicon(__dirname + '/site/resources/images/favicon.ico')); 
+
 //using static files
-app.use(express.static(path.join(__dirname,'./site/resources')));
+app.use(express.static(path.join(__dirname,'/site/resources')));
 
 //configure app
-app.set('views', path.join(__dirname, './site/views'));
+app.set('views', path.join(__dirname, '/site/views'));
 app.set('view engine', 'ejs');
 app.set('view cache',false);
 
